407725449ff33e326a92f874c4bb0b67e61fea4e
GaboRomo96 <gromo1700@gmail.com>
2017-05-31 20:32:07 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -11,3 +11,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+/*
+public class ISBN {
+
+
+    private static boolean isbn10(String isbn){
+            
+            String mcdD10;
+            int suma = 0;
+            
+            for (int d = 0; d < 10; d++){
+                mcdD10 = isbn.substring(d, d+1);
+                if(d < 9 || mcdD10 != "X"){
+                    suma += Integer.parseInt(mcdD10)* (10 - d);                    
+                }else {
+                    suma += 10;
+                }                
+            }            
+            return (suma % 11 == 0);
+        }
+        
+    private static boolean isbn13(String isbn){
+            
+            int mcdD13;
+            int suma = 0;
+            
+            for (int d = 0; d < 13; d++){
+                mcdD13 = Integer.parseInt(isbn.substring(d, d+1));
+                if (d % 2 == 0){
+                    suma += mcdD13 *1;                    
+                }else {
+                    suma += mcdD13 *3;
+                }
+            }            
+            return (suma % 11 == 0);
+        }
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String isbn;
+        
+        System.out.println("Ingresa un ISBN");
+        isbn = sc.nextLine();
+        sc.close();
+        
+        isbn = isbn.replaceAll("( |-)", "");
+        
+        
+        boolean isValid = false;
+        if(isbn.length() == 10){
+            isValid = isbn10(isbn);                        
+        }else if(isbn.length() == 13){
+            isValid = isbn13(isbn);            
+        }else {
+            isValid = false;
+        }
+        
+        if(isValid){
+            System.out.println(isbn + ": Es un ISBN Valido!");
+        }else {
+            System.out.println(isbn + ": No es un isbn Valido");            
+        }
+    }                    
+}    
+
+*/
