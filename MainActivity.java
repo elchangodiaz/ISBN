eb5072cb5ade62b45c7a2cc7b22b0bfbc7434c29
elchangodiaz <elchangodiaz1@gmail.com>
2017-06-01 17:57:09 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.javier.isbn;
 
 import android.content.Context;
+import android.media.MediaCodec;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,25 +39,49 @@ public class MainActivity extends AppCompatActivity {
 
     public class ISBN {
 
-        private boolean isbn10(String isbn) {
+        public static final int ISBN13 = 13;
 
-            String mcdD10;
-            int suma = 0;
+        public static final int ISBN10 = 10;
 
-            for (int d = 0; d < 10; d++) {
-                mcdD10 = isbn.substring(d, d + 1);
-                if (d < 9 || mcdD10 != "X") {
-                    suma += Integer.parseInt(mcdD10) * (10 - d);
-                } else {
-                    suma += 10;
-                }
+        public boolean validar(String isbn){
+            if (isbn == null)
+                Toast.makeText(getApplicationContext(), "Ingrese un ISBN valido", Toast.LENGTH_SHORT);
+            //if (!Pattern.matches("^$d}%&()//*?!@¡", isbn )) return false;
+
+
+            if (isbn.length() == 13) return ISBN13Valido();
+           // else if (isbn.length() == 10) return ISBN10Valido ();
+            else Toast.makeText(getApplicationContext(), "ISBN invalido", Toast.LENGTH_SHORT);
+            return false;
+
+        }
+
+     public boolean ISBN13Valido(String numero){
+         if (numero == null)
+             Toast.makeText(getApplicationContext(), "ISBN no valido", Toast.LENGTH_SHORT);
+         if (!Pattern.matches("^\\d{" + ISBN13 + "}$", numero)) throw new IllegalArgumentException();
+         char [] digits = numero.toCharArray();
+         final int miDigito = calcularISBN13CheckDigito (digits);
+         int checkDigito = digits[ISBN13 - 1] - '0';
+         return miDigito == 10 && checkDigito == 0 || miDigito == checkDigito;
+     }
+
+        public int calcularISBN13CheckDigito(char[] digits) {
+            if (digits == null) throw new NullPointerException();
+            if (digits.length != ISBN13 && digits.length != ISBN13 - 1) throw new IllegalArgumentException();
+            for (char c : digits) {
+                if (c < '0' || '9' < c) throw new IllegalArgumentException();
             }
-            return (suma % 11 == 0);
+
+            int[] multip = {1, 3};
+            int suma = 0;
+            for (int i = 0; i < ISBN13 - 1; ++i) {
+                suma += (digits[i] - '0') * multip[i % 2];
+            }
+            return 10 - suma % 10;
         }
 
 
-    }
-}
 
 
 
